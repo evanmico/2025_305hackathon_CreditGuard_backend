3d1328175e792b4c738a37ffc5ea988e640d7ae4
PrimarchPaul <preye032@fiu.edu>
2025-03-30 16:33:37 -0400

    added database connection. function data retreival and storage. pls help ivan

---
+++ config/azure.js
@@ -0,0 +1,18 @@
+import mysql2 from "mysql2/promise";
+
+const connectDB = mysql2.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+});
+
+export default connectDB;
